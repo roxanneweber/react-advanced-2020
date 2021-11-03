bb8e9a4a34c8169a38098ae8398fcc9a08d748e7
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-03 12:01:52 -0500

    set up spread operator for object useState changes

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,12 +7,18 @@ const UseStateObject = () => {
 		age: '24',
 		message: 'random message',
 	});
-	console.log(person);
-	const changeMessage = () => {};
+	const changeMessage = () => {
+		setPerson({ ...person, message: 'hello world' });
+	};
 
 	return (
 		<>
-			<h2>Use State Object</h2>
+			<h3>{person.name}</h3>
+			<h3>{person.age}</h3>
+			<h3>{person.message}</h3>
+			<button className='btn' onClick={changeMessage}>
+				change message
+			</button>
 		</>
 	);
 };
