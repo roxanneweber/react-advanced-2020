42f11da4e414a7de5dfecb8542b965e363385925
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-03 00:11:32 -0500

    setup object and how to change one item in the object with a handler

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const [person, setPerson] = useState({
+		name: 'peter',
+		age: '24',
+		message: 'random message',
+	});
+
+	const changeMessage = () => {
+		setPerson({ ...person, message: 'hello world' });
+		console.log(person);
+	};
+
+	return (
+		<>
+			<h3>Name: {person.name}</h3>
+			<h3>Age: {person.age}</h3>
+			<h3>Message: {person.message}</h3>
+			<button className='btn' onClick={changeMessage}>
+				change message
+			</button>
+		</>
+	);
 };
 
 export default UseStateObject;
